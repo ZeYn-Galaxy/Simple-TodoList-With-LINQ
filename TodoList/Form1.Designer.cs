88e53aed3082946d9a3d666158960989ed95cc12
ZeYn-Galaxy <shafwan066@gmail.com>
2024-05-12 21:31:38 +0800

    Add project files.

---
+++ TodoList/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace TodoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.table = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputTask = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.r1 = new System.Windows.Forms.RadioButton();
+            this.r2 = new System.Windows.Forms.RadioButton();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.inputTitle = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.inputSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbStatus = new System.Windows.Forms.ComboBox();
+            this.date1 = new System.Windows.Forms.DateTimePicker();
+            this.date2 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.table.Location = new System.Drawing.Point(0, 421);
+            this.table.Name = "table";
+            this.table.RowHeadersWidth = 51;
+            this.table.RowTemplate.Height = 24;
+            this.table.Size = new System.Drawing.Size(1103, 322);
+            this.table.TabIndex = 0;
+            this.table.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.table_CellMouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Todo List";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.inputTitle);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.btnSubmit);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.inputTask);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(18, 62);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(626, 289);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tambah Data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Task";
+            // 
+            // inputTask
+            // 
+            this.inputTask.Location = new System.Drawing.Point(59, 62);
+            this.inputTask.Multiline = true;
+            this.inputTask.Name = "inputTask";
+            this.inputTask.Size = new System.Drawing.Size(296, 121);
+            this.inputTask.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.r2);
+            this.groupBox2.Controls.Add(this.r1);
+            this.groupBox2.Location = new System.Drawing.Point(383, 30);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(223, 155);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Status";
+            // 
+            // r1
+            // 
+            this.r1.AutoSize = true;
+            this.r1.Location = new System.Drawing.Point(7, 34);
+            this.r1.Name = "r1";
+            this.r1.Size = new System.Drawing.Size(79, 20);
+            this.r1.TabIndex = 0;
+            this.r1.TabStop = true;
+            this.r1.Text = "Ongoing";
+            this.r1.UseVisualStyleBackColor = true;
+            // 
+            // r2
+            // 
+            this.r2.AutoSize = true;
+            this.r2.Location = new System.Drawing.Point(136, 34);
+            this.r2.Name = "r2";
+            this.r2.Size = new System.Drawing.Size(79, 20);
+            this.r2.TabIndex = 1;
+            this.r2.TabStop = true;
+            this.r2.Text = "FInished";
+            this.r2.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(10, 208);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(596, 28);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Tambah";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(10, 237);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(154, 30);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Title";
+            // 
+            // inputTitle
+            // 
+            this.inputTitle.Location = new System.Drawing.Point(59, 30);
+            this.inputTitle.Multiline = true;
+            this.inputTitle.Name = "inputTitle";
+            this.inputTitle.Size = new System.Drawing.Size(296, 26);
+            this.inputTitle.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 392);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Search";
+            // 
+            // inputSearch
+            // 
+            this.inputSearch.Location = new System.Drawing.Point(60, 385);
+            this.inputSearch.Multiline = true;
+            this.inputSearch.Name = "inputSearch";
+            this.inputSearch.Size = new System.Drawing.Size(296, 30);
+            this.inputSearch.TabIndex = 7;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(362, 385);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(76, 30);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.date2);
+            this.groupBox3.Controls.Add(this.date1);
+            this.groupBox3.Controls.Add(this.cbStatus);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.btnFilter);
+            this.groupBox3.Location = new System.Drawing.Point(681, 72);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(400, 279);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Filter";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(17, 134);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(358, 28);
+            this.btnFilter.TabIndex = 7;
+            this.btnFilter.Text = "Apply";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Status";
+            // 
+            // cbStatus
+            // 
+            this.cbStatus.FormattingEnabled = true;
+            this.cbStatus.Location = new System.Drawing.Point(17, 46);
+            this.cbStatus.Name = "cbStatus";
+            this.cbStatus.Size = new System.Drawing.Size(358, 24);
+            this.cbStatus.TabIndex = 8;
+            this.cbStatus.SelectedIndexChanged += new System.EventHandler(this.cbStatus_SelectedIndexChanged);
+            // 
+            // date1
+            // 
+            this.date1.Location = new System.Drawing.Point(17, 87);
+            this.date1.Name = "date1";
+            this.date1.Size = new System.Drawing.Size(140, 22);
+            this.date1.TabIndex = 9;
+            this.date1.ValueChanged += new System.EventHandler(this.date1_ValueChanged);
+            // 
+            // date2
+            // 
+            this.date2.Location = new System.Drawing.Point(235, 87);
+            this.date2.Name = "date2";
+            this.date2.Size = new System.Drawing.Size(140, 22);
+            this.date2.TabIndex = 10;
+            this.date2.ValueChanged += new System.EventHandler(this.date2_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(183, 92);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "And";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1103, 743);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.inputSearch);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.table);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Todo List";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton r2;
+        private System.Windows.Forms.RadioButton r1;
+        private System.Windows.Forms.TextBox inputTask;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputTitle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox inputSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker date2;
+        private System.Windows.Forms.DateTimePicker date1;
+        private System.Windows.Forms.ComboBox cbStatus;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnFilter;
+    }
+}
+
---
+++ TodoList/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TodoList
+{
+    public partial class Form1 : Form
+    {
+        private bool dateChanged = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool alert(string message, string title) {
+            if (string.IsNullOrEmpty(message) || string.IsNullOrWhiteSpace(message)) {
+                MessageBox.Show(title + " cannot be empty!", "Input Validation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return true;
+            }
+            return false;
+        }
+
+        private void search(string keyword)
+        {
+
+            using (var context = new SimpleTodoListEntities())
+            {
+
+                var query = from item in context.tb_task
+                            where item.title.Contains(keyword)
+                            select new
+                            {
+                                ID = item.id,
+                                Title = item.title,
+                                Task = item.task,
+                                Status = item.status,
+                                Created_At = item.created_at
+                            };
+
+                table.Columns.Clear();
+                table.DataSource = query.ToArray();
+
+                DataGridViewButtonColumn delete = new DataGridViewButtonColumn();
+                delete.HeaderText = "";
+                delete.Text = "Delete";
+                delete.UseColumnTextForButtonValue = true;
+
+                DataGridViewButtonColumn update = new DataGridViewButtonColumn();
+                update.HeaderText = "";
+                update.Text = "Update";
+                update.UseColumnTextForButtonValue = true;
+
+                table.Columns.AddRange(delete, update);
+
+            }
+        }
+
+        private void filter()
+        {
+
+            using (var context = new SimpleTodoListEntities())
+            {
+
+                var status = cbStatus.SelectedItem;
+                var query = from item in context.tb_task
+                            where (cbStatus.SelectedIndex == 0 || item.status == status.ToString())
+                            && (!dateChanged || DbFunctions.TruncateTime(item.created_at) >= DbFunctions.TruncateTime(date1.Value) && DbFunctions.TruncateTime(item.created_at) <= DbFunctions.TruncateTime(date2.Value))
+                            select new
+                            {
+                                ID = item.id,
+                                Title = item.title,
+                                Task = item.task,
+                                Status = item.status,
+                                Created_At = item.created_at
+                            };
+
+                table.Columns.Clear();
+                table.DataSource = query.ToArray();
+
+                DataGridViewButtonColumn delete = new DataGridViewButtonColumn();
+                delete.HeaderText = "";
+                delete.Text = "Delete";
+                delete.UseColumnTextForButtonValue = true;
+
+                DataGridViewButtonColumn update = new DataGridViewButtonColumn();
+                update.HeaderText = "";
+                update.Text = "Update";
+                update.UseColumnTextForButtonValue = true;
+
+                table.Columns.AddRange(delete, update);
+
+            }
+
+            cbStatus.SelectedIndex = 0;
+            date1.ResetText();
+            date2.ResetText();
+            dateChanged = false;
+            btnFilter.Enabled = false;
+        }
+
+        private void refresh()
+        {
+            dateChanged = false;
+            inputTask.Clear();
+            inputTitle.Clear();
+            r1.Checked = true;
+            r2.Checked = false;
+            btnCancel.Enabled = false;
+            btnSubmit.Text = "Tambah";
+            btnSubmit.Tag = "";
+
+            using (var context = new SimpleTodoListEntities())
+            {
+
+                var query = from item in context.tb_task
+                            select new { 
+                                ID = item.id,
+                                Title = item.title,
+                                Task = item.task,
+                                Status = item.status,
+                                Created_At = item.created_at
+                            };
+
+                table.Columns.Clear();
+                table.DataSource = query.ToArray();
+
+                DataGridViewButtonColumn delete = new DataGridViewButtonColumn();
+                delete.HeaderText = "";
+                delete.Text = "Delete";
+                delete.UseColumnTextForButtonValue = true;
+
+                DataGridViewButtonColumn update = new DataGridViewButtonColumn();
+                update.HeaderText = "";
+                update.Text = "Update";
+                update.UseColumnTextForButtonValue = true;
+
+                table.Columns.AddRange(delete, update);
+
+            }
+        }
+
+        private void add()
+        {
+            if (alert(inputTitle.Text, "Title")) { return; }
+            if (alert(inputTask.Text, "Task")) { return; }
+
+            using (var context = new SimpleTodoListEntities())
+            {
+                var selected = r1.Checked ? r1.Text : r2.Text;
+                var newData = new tb_task();
+                newData.title = inputTitle.Text;
+                newData.task = inputTask.Text;
+                newData.status = selected;
+                newData.created_at = DateTime.Now;
+
+                context.tb_task.Add(newData);
+                context.SaveChanges();
+                refresh();
+
+            }
+        }
+
+        private void update()
+        {
+            if (alert(inputTitle.Text, "Title")) { return; }
+            if (alert(inputTask.Text, "Task")) { return; }
+
+            using (var context = new SimpleTodoListEntities())
+            {
+                var selected = r1.Checked ? r1.Text : r2.Text;
+                var id = int.Parse(btnSubmit.Tag.ToString());
+                var newData = context.tb_task.SingleOrDefault(item => item.id == id);
+                newData.title = inputTitle.Text;
+                newData.task = inputTask.Text;
+                newData.status = selected;
+                newData.update_at = DateTime.Now;
+                context.SaveChanges();
+                refresh();
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            refresh();
+            cbStatus.Items.AddRange(new string[] {"Choose Status", "Ongoing", "Finished"});
+            cbStatus.SelectedIndex = 0;
+            btnFilter.Enabled = false;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (btnSubmit.Text == "Tambah")
+            {
+                add();
+            } else
+            {
+                update();
+            }
+        }
+
+        private void table_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex == -1) { return; }
+            DataGridViewRow row = table.Rows[e.RowIndex];
+            var id = int.Parse(row.Cells[0].Value.ToString());
+            if (e.ColumnIndex == 5)
+            {
+                DialogResult dialog = MessageBox.Show("Are you sure delete, the data?", "Delete Option", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialog == DialogResult.Yes)
+                {
+                    using (var context = new SimpleTodoListEntities())
+                    {
+                        var data = context.tb_task.SingleOrDefault(item => item.id == id);
+                        context.tb_task.Remove(data);
+                        context.SaveChanges();
+                        refresh();
+                    }
+                }
+                return;
+            }
+
+            if (e.ColumnIndex == 6)
+            {
+                inputTask.Text = row.Cells[2].Value.ToString();
+                inputTitle.Text = row.Cells[1].Value.ToString();
+                r1.Checked = row.Cells[3].Value.ToString().Equals("Ongoing");
+                r2.Checked = !row.Cells[3].Value.ToString().Equals("Ongoing");
+                btnCancel.Enabled = true;
+                btnSubmit.Text = "Update";
+                btnSubmit.Tag = id;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            inputTask.Clear();
+            inputTitle.Clear();
+            r1.Checked = true;
+            r2.Checked = false;
+            btnCancel.Enabled = false;
+            btnSubmit.Text = "Tambah";
+            btnSubmit.Tag = "";
+            dateChanged = false;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(inputSearch.Text) || string.IsNullOrWhiteSpace(inputSearch.Text))
+            {
+                refresh();
+                return;
+            }
+            search(inputSearch.Text);
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            filter();
+        }
+
+        private void date1_ValueChanged(object sender, EventArgs e)
+        {
+            dateChanged = true;
+            btnFilter.Enabled = true;
+        }
+
+        private void date2_ValueChanged(object sender, EventArgs e)
+        {
+            dateChanged = true;
+            btnFilter.Enabled = true;
+        }
+
+        private void cbStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnFilter.Enabled = true;
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
